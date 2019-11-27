da30669d1811964931fd4c49662610bfbfcd34b1
Andrew <andrii.pravdyvyi@gmail.com>
2019-11-27 20:57:56 +0200

    Server.js changed

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 //Web App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello world DEV namespace\n');
 });
 
 app.listen(PORT, HOST);
